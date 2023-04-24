27b7df222005f063ee04243236ef5b8ae9725f4d
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-04-24 09:27:18 +0200

    Make migrations match E/R

--- database/migrations/2023_04_24_065530_create_ratings_table.php
+++ database/migrations/2023_04_24_065530_create_ratings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
+            $table->integer('rating');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('product_id')->constrained('products');
             $table->timestamps();
         });
     }
