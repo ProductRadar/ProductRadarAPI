76783d348adee03dcd9cb0664a1cfd2b3f742571
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-04-24 12:24:16 +0200

    Added on delete cascade to ratings and favorites

--- database/migrations/2023_04_24_065530_create_ratings_table.php
+++ database/migrations/2023_04_24_065530_create_ratings_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
             $table->integer('rating');
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
