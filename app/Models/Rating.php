93d2bb7eef245b9323b50ee6b1bdef8ef1de28c8
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-04-24 09:06:43 +0200

    Make Models match E/R

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'rating',
+        'product_id',
+        'user_id',
+    ];
 }
