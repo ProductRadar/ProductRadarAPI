8dae487683f8f600be939ed4647a8aebbc866a79
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-04-24 10:25:58 +0200

    Setup basic routes for api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/rating', \App\Http\Controllers\RatingController::class);
+Route::apiResource('/favorite', \App\Http\Controllers\FavoriteController::class);
+Route::apiResource('/product', \App\Http\Controllers\ProductController::class);
+Route::apiResource('/user', \App\Http\Controllers\UserController::class);
