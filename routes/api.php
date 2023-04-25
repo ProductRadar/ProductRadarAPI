d1e6228edef598135b5893efa36a3a6f978e6255
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-04-25 14:18:46 +0200

    Changed product crud rights

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/rating', \App\Http\Controllers\RatingController::class)->middleware('auth:sanctum');
 Route::apiResource('/favorite', \App\Http\Controllers\FavoriteController::class)->middleware("auth:sanctum");
-Route::apiResource('/product', \App\Http\Controllers\ProductController::class)->middleware("auth:sanctum");
+
+Route::group(array('namespace' => 'Front', 'prefix'=>''),function() {
+    Route::get('/product', '\App\Http\Controllers\ProductController@index')->name('product.index');
+    Route::post('/product', '\App\Http\Controllers\ProductController@store')->name('product.store')->middleware("auth:sanctum");
+    Route::put('/product/{product}', '\App\Http\Controllers\ProductController@update')->name('auth.update')->middleware("auth:sanctum");
+    Route::delete('/product', '\App\Http\Controllers\ProductController@delete')->name('auth.delete')->middleware("auth:sanctum");
+});
+
 Route::group(array('namespace' => 'Front', 'prefix'=>''),function() {
     Route::post('auth/login', '\App\Http\Controllers\AuthController@login')->name('auth.login');
     Route::post('auth/register', '\App\Http\Controllers\AuthController@register')->name('auth.register');
